75531002362296bda8633902bf1a189f7854b6f3
yellofi <yunseob1102@gmail.com>
2022-05-24 10:16:15 +0900

    set run config of train.py

--- tumor_region_seg/train.sh
+++ tumor_region_seg/train.sh
@@ -1,3 +1,3 @@
 cd ./c-MET/c-met_yellofi/tumor_region_seg
 
-python3 train.py --data_type 'samsung' --fold 1 --local_rank 0 1 2 3 4 5 6 7 --n_epoch 100 --batch_size 144
+python3 train.py --data_type 'samsung' --fold 1 --local_rank 0 1 2 3 4 5 6 7 --n_epoch 200 --batch_size 144 --model_dir '/mnt/hdd1/model/Lung_c-MET IHC_scored/UNet/06_baseline_samsung_data'
