dcc70be366667151989f7037a06255d913288e32
yellofi <yunseob1102@gmail.com>
2022-05-24 10:14:33 +0900

    make it into single net and ensemble seperately

--- tumor_region_seg/tumor_seg_inference_single_net.py
+++ tumor_region_seg/tumor_seg_inference_single_net.py
@@ -1,12 +1,12 @@
 import os
-from PIL import Image
 import numpy as np
+import cv2
+from PIL import Image
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import torch
-from torch.utils.data import DataLoader
-from torchvision import transforms
-from model import UNet
-from data_utils import RGB2GH
+from model import *
+from data_utils import *
 import time
 import argparse
 
@@ -15,10 +15,12 @@ def parse_arguments():
 
     parser.add_argument('--patch_dir', type=str, 
                         default='/mnt/hdd1/c-MET_datasets/SLIDE_DATA/록원재단/AT2/C-MET_slide/patch_on_ROI/sobel+blurrity_check', help='patch directory')
-    parser.add_argument('--model_dir', type=str, 
-                        default='./model', help='model directory (5 models corresponing to each fold)')
+    parser.add_argument('--model_path', type=str, 
+                        default='/mnt/hdd1/model/Lung_c-MET IHC_scored/UNet/06_baseline_samsung_data/1-fold/checkpoint/model_epoch159.pth', help='model path (*.pth)')
+    parser.add_argument('--save_dir', action="store", type=str,
+                        default='/mnt/hdd1/model/Lung_c-MET IHC_scored/UNet/06_baseline_samsung_data/1-fold/output', help='directory where results would be saved')
 
-    parser.add_argument('--local_rank', type=int, default=7 , help='local rank (gpu idx)')
+    parser.add_argument('--local_rank', type=int, nargs='+', default=[0], help='local rank')
     
     parser.add_argument('--input_type', type=str, default='RGB')
     parser.add_argument('--batch_size', type=int, default=1)
@@ -92,89 +94,86 @@ class Normalization(object):
         data['input'] = input
         return data
 
-def net_test_load(ckpt_dir, net, epoch = 0, device = torch.device('cuda:0')):
-    if not os.path.exists(ckpt_dir): # 저장된 네트워크가 없다면 인풋을 그대로 반환
-        epoch = 0
-        return net
-    
-    ckpt_lst = os.listdir(ckpt_dir) # ckpt_dir 아래 있는 모든 파일 리스트를 받아온다
-    ckpt_lst.sort(key = lambda f : int(''.join(filter(str.isdigit,f))))
+def net_test_load(model_path, net, device=None):
+    if device != None:
+        dict_model = torch.load(model_path, map_location=device)
+        net.load_state_dict(dict_model['net'])
+    else:
+        dict_model = torch.load(model_path, map_location='cpu')
+        net_state_dict_ = OrderedDict()
+        for k, v in dict_model['net'].items():
+            name  = k.replace("module.", "")
+            net_state_dict_[name] = v
+        net.load_state_dict(net_state_dict_)
 
-    print(f'{ckpt_lst[epoch-1]}')
-    dict_model = torch.load('%s/%s' % (ckpt_dir,ckpt_lst[epoch-1]), map_location=device)
-    
-    net.load_state_dict(dict_model['net'])
-
+    print('model: ', model_path)
     return net
 
+def sigmoid(z):
+    return 1/(1+np.e**(-z))
+
+def make_heatmap(output):
+    # output = output-output.min()
+    # output = output/output.max()
+
+    output = sigmoid(output)
+    heatmap = cm.jet(output)[:, :, :3]
+    return heatmap.astype('float32')
 
 if __name__ == '__main__':
 
     args = parse_arguments()
 
     print('Load Models...')
-    
+
     rank = args.local_rank
-    model_dir = args.model_dir
-    batch_size = args.batch_size
+    model_path = args.model_path
     input_type = args.input_type
+
+    if len(rank) != 1:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        net = UNet(input_type, DataParallel=True)
+        net = net_test_load(model_path, net)
+        net = torch.nn.DataParallel(net, device_ids=rank)
+        net = net.to(device)
+    else:
+        # single gpu -> device map location으로 불러와야 gpu 0을 안 씀
+        device = torch.device(f'cuda:{rank}')
+        net = UNet(input_type).to(device)
+        net = net_test_load(model_path, net, device=device) 
+        torch.cuda.set_device(rank)
+        device = torch.device(f'cuda:{rank}')
+
     patch_dir = args.patch_dir
     
-    torch.cuda.set_device(rank)
-    device = torch.device(f'cuda:{rank}')
-
-    model_select = []
-
-    model_dir = '/mnt/hdd1/model/Lung_c-MET IHC_scored/UNet/01_5-f_cv_baseline'
-    model_select = [207, 208, 263, 290, 285] 
-
-    # model_dir = '/mnt/hdd1/model/Lung_c-MET IHC_scored/UNet/05_5-f_cv_GH'
-    # model_select = [124, 114, 132, 103, 107] # 200 epoch
-  
-    k_fold = 5
-    nets = []
-
-    for i in range(k_fold):
-        print(f'{i+1}-fold - ', end = '')
- 
-        ckpt_dir = f'{model_dir}/{i+1}-fold/checkpoint'
-
-        net = UNet(input_type).to(device)
-        # print(net)
-        
-        if len(model_select) == k_fold:
-            net = net_test_load(ckpt_dir = ckpt_dir, net = net, epoch = model_select[i], device=device)
-        else: 
-            net = net_test_load(ckpt_dir = ckpt_dir, net = net, epoch = 0, device=device)
-
-        nets.append(net)
-
     slide_list = sorted([f for f in os.listdir(patch_dir) if os.path.isdir(os.path.join(patch_dir, f))])
+
+    print('Inference...')
 
     fn_tonumpy = lambda x : x.to('cpu').detach().numpy().transpose(0, 2, 3, 1)
     fn_denorm = lambda x, mean, std : (x*std) + mean
     fn_norm = lambda x : (x-x.min())/(x.max()-x.min())
     fn_classifier = lambda x : 1.0 * (x > 0.5)
-    
-    print('Inference...')
 
+    save_dir = args.save_dir
+    batch_size = args.batch_size
+    
     total_time = 0
+
     for slide_id in slide_list:
         
         start_time = time.time()
         data_dir = os.path.join(patch_dir, slide_id, '200x')
-        mask_save_dir = f'{model_dir}/output/slide/{slide_id}'
-        plot_save_dir = mask_save_dir + '/plot'
+        mask_save_dir = f'{save_dir}/slide/{slide_id}'
+        # plot_save_dir = mask_save_dir + '/plot'
 
-        try: os.makedirs(mask_save_dir); os.makedirs(plot_save_dir)
+        try: os.makedirs(mask_save_dir)
         except: pass
         
         transform = transforms.Compose([Normalization(mean=0.5, std=0.5), ToTensor()])
-        # dataset = Dataset(data_dir=data_dir, transform = transform)
         dataset = Dataset(data_dir, transform, input_type)
-        loader = DataLoader(dataset, batch_size = batch_size, shuffle=False)
+        loader = DataLoader(dataset, batch_size, shuffle=False)
 
-        results = []
         with torch.no_grad(): 
             net.eval() 
             
@@ -185,39 +184,26 @@ if __name__ == '__main__':
                     img = data['img']
                 
                 img_id = data['id']
-                
-                outputs = []
-                for net in nets:
-                    output = net(input)
-                    outputs.append(np.squeeze(fn_tonumpy(fn_norm(output)), axis=-1))
-                    # outputs.append(np.squeeze(fn_tonumpy(output), axis=-1))
+                output = net(input)
+
+                output = np.squeeze(fn_tonumpy(output), axis=-1)
+                pred = fn_classifier(output).astype('float32')
 
                 if input_type == 'GH':
                     input = img.to('cpu').detach().numpy()
-                    # print(input.shape, input.dtype, input.max(), input.min())
                 else:
                     input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
-                output_avg = np.mean(np.asarray(outputs), axis = 0)
-                final_pred = fn_classifier(output_avg)
 
-                results.append((img_id, input, final_pred, output_avg))
+                for img_id_, img_, pred_, output_ in zip(img_id, input, pred, output):
+  
+                    heatmap = make_heatmap(output_)
+                    overlay = cv2.addWeighted(img_, 0.7, heatmap, 0.3, 0)
+                    overlay = Image.fromarray(np.uint8(overlay*255))
+                    overlay.save(f'{mask_save_dir}/{img_id_[:-4]}_heatmap_overlay.jpg')
 
-        for (img_id, img, pred, output) in results:
-            for i in range(batch_size):
-                plt.figure(figsize = (20, 10))
-                plt.subplot(1, 3, 1)
-                plt.imshow(img[i])
-                plt.subplot(1, 3, 2)
-                plt.imshow(img[i])
-                plt.imshow(output[i], cmap ='jet', alpha = 0.3)
-                plt.subplot(1, 3, 3)
-                plt.imshow(img[i])
-                plt.imshow(pred[i], alpha = 0.3)
-                plt.savefig(f'{plot_save_dir}/{img_id[i][:-4]}_prediction_overlay.jpg', bbox_inches = 'tight')
-                plt.close()
-
-                pred_ = Image.fromarray(np.uint8(pred[i]*255))
-                pred_.save(f'{mask_save_dir}/{img_id[i][:-4]}_predicted_tumor_region.png')
+                    pred_ = Image.fromarray(np.uint8(pred_*255))
+                    pred_.save(f'{mask_save_dir}/{img_id_[:-4]}_predicted_tumor_region.png')
+        
 
         taken = time.time() - start_time
         print(f'{slide_id} | #: {len(os.listdir(data_dir))} | time: {round(taken, 2)} sec')
