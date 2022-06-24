87f27e22fffd08a8ba62968c2aa2f30c5d01d6da
yellofi <yunseob1102@gmail.com>
2022-06-24 10:00:37 +0900

    define 'make_patch_' here

--- patch_gen/model_laplacian/__init__.py
+++ patch_gen/model_laplacian/__init__.py
@@ -8,3 +8,13 @@ from .laplacian import *
 def load_model():
     model = Laplacian()
     return model
+
+def make_patch_(patch):
+    """
+    corresponding smaller sized gray-scale patch for blurrity check 
+    """
+    patch = np.array(patch)
+    patch = patch.astype(np.float32)
+    patch = np.expand_dims(patch, axis=0)
+    patch = np.ascontiguousarray(patch, dtype=np.float32)
+    return patch
