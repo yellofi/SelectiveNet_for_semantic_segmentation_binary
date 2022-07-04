888b34da368b4160eb8139e01c38984cb25db32d
yellofi <yunseob1102@gmail.com>
2022-07-04 17:35:21 +0900

    add selective in UNet_B

--- tumor_seg/model.py
+++ tumor_seg/model.py
@@ -15,8 +15,10 @@ def CBR_2D(in_ch, out_ch, k_size = 3, stride = 1, padding = 1, bias = True):
     return conv_block
 
 class UNet_B(nn.Module):
-    def __init__(self, input_type = 'RGB'):
+    def __init__(self, input_type = 'RGB', selective = False):
         super(UNet_B, self).__init__()
+
+        self.selective = selective
 
         if 'RGB' in input_type:
             input_ch = 3
@@ -58,6 +60,10 @@ class UNet_B(nn.Module):
         self.decoder_layer_1_1 = CBR_2D(in_ch=64, out_ch=64)
         self.conv1x1 = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1)
 
+        if self.selective:
+            self.conv_select = nn.Conv2d(64, 1, 1)
+            self.conv_aux = nn.Conv2d(64, 1, 1)
+
     def forward(self, x):
         enc1_1 = self.encoder_layer_1_1(x)
         enc1_2 = self.encoder_layer_1_2(enc1_1)
@@ -86,11 +92,15 @@ class UNet_B(nn.Module):
         dec1_2 = self.decoder_layer_1_2(unpool1)
         dec1_1 = self.decoder_layer_1_1(dec1_2)
 
+        dec1_1 = dec1_1.clone().detach()
         output = self.conv1x1(dec1_1)
 
-        return torch.squeeze(output, 1) # (N, 1, H, W) -> (N, H, W) label shape
-     
-
+        if self.selective:
+            select = self.conv_select(dec1_1) # (N, 1, H, W)
+            aux = self.conv_aux(dec1_1) # (N, 1, H, W)
+            return torch.squeeze(output, 1), torch.squeeze(select, 1), torch.squeeze(aux, 1)
+        else:
+            return torch.squeeze(output, 1) # (N, 1, H, W) -> (N, H, W) label shape
 
 class UNet(nn.Module):
     def __init__(self, input_type = 'RGB', n_cls = 2, selective = False):
